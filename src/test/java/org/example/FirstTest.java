4713ae9d89bc74d51c1adfefda309c43cb62ac93
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-14 09:37:23 -0300

    Implements Arithmetic exception - Not divide numbers by 0

--- src/test/java/org/example/FirstTest.java
+++ src/test/java/org/example/FirstTest.java
@@ -53,6 +53,13 @@ public class FirstTest {
         Assertions.assertThat(result).isEqualTo(2);
     }
 
+    @Test(expected = ArithmeticException.class)
+    public void notAllowedDivideNumbersByZero(){
+        //scenario
+        int num1 = 10, num2 = 0;
+        //executor
+        calculator.divide(num1, num2);
+    }
     @Test
     public void mustMultiplyTwoNumbers(){
         //scenario
@@ -73,23 +80,14 @@ class Calculator{
     }
 
     int subtract(int num, int num2) {
-        if(num < 0 || num2 < 0){
-            throw new RuntimeException("It is not allowed to subtract negative numbers");
-        }
         return  num - num2;
     }
 
     int divide(int num, int num2){
-        if (num < 0 || num2 <= 0){
-            throw new RuntimeException("Division by negative or zero numbers is not allowed.");
-        }
         return num / num2;
     }
 
     int multiply(int num, int num2){
-        if (num < 0 || num2 < 0){
-            throw new RuntimeException("Multiplying numbers by zero is not allowed");
-        }
         return num * num2;
     }
 }
